5fa173bb68cbfafe7553755c62261972585373fa
Rasel <mdraselm413@gmail.com>
2022-09-29 22:16:56 +0600

    icon added

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css'
 
 const Product = ({product, handleAddToCart}) => {
@@ -17,7 +19,8 @@ const Product = ({product, handleAddToCart}) => {
                 <p><small>Ratings: {ratings} stars</small></p>
             </div>
             <button onClick={() => handleAddToCart(product)} className='btn-cart'>
-                <p>Add To Cart</p>
+                <p className='btn-text'>Add To Cart</p>
+                <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
             </button>
         </div>
     );
