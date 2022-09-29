efbd13253a67802b922ed8f0a4f363e1fedb24d4
Rasel <mdraselm413@gmail.com>
2022-09-29 19:44:25 +0600

    next added

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Product.css'
 
-const Product = (props) => {
+const Product = ({product, handleAddToCart}) => {
     // console.log(props.product);
+    // const {product, handleAddToCart} = props;
     
-    const {name, img, seller, price, ratings} = props.product;
+    const {name, img, seller, price, ratings} = product;
     
     return (
         <div className='product'>
@@ -15,7 +16,7 @@ const Product = (props) => {
                 <p><small>Seller: {seller}</small></p>
                 <p><small>Ratings: {ratings} stars</small></p>
             </div>
-            <button onClick={() => props.handleAddToCart(props.product)} className='btn-cart'>
+            <button onClick={() => handleAddToCart(product)} className='btn-cart'>
                 <p>Add To Cart</p>
             </button>
         </div>
